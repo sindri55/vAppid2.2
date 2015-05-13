94e031db70d6d09f7fe2f03a39b4003979d72a81
Ragnar Borgþór Ragnarsson <ragnarbr1992@gmail.com>
2015-05-13 21:00:21 +0000

    Adding adapter..

---
+++ sample/src/main/java/com/github/devnied/emvnfccard/adapter/MobileArrayAdapter.java
@@ -0,0 +1,63 @@
+package com.github.devnied.emvnfccard.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.devnied.emvnfccard.R;
+
+
+/**
+ * Created by dabbi on 13.5.2015.
+ */
+
+public class MobileArrayAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+
+    public MobileArrayAdapter(Context context, String[] values) {
+        super(context, R.layout.fundraiser, R.id.label, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.fundraiser, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
+        textView.setText(values[position]);
+
+
+        // Change icon based on name
+        String s = values[position];
+
+        //System.out.println(s);
+
+        if (s.equals("Landsbjörg")) {
+            imageView.setImageResource(R.drawable.landsbjorg);
+        } else if (s.equals("Blái Naglinn")) {
+            imageView.setImageResource(R.drawable.blai);
+        } else if (s.equals("SÁÁ Álfurinn")) {
+            imageView.setImageResource(R.drawable.saa_alfurinn);
+        } else {
+            imageView.setImageResource(R.drawable.kr);
+        }
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "You clicked " + values[position], Toast.LENGTH_LONG).show();
+
+            }
+        });
+        return rowView;
+
+    }
+}
