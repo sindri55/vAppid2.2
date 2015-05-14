d3eed50a3378630213e5659835bf5476d6a262ce
Ragnar Borgþór Ragnarsson <ragnarbr1992@gmail.com>
2015-05-14 16:09:12 +0000

    Added the sidebar to the paid activity..

--- sample/src/main/java/com/github/devnied/emvnfccard/activity/testActivity.java
+++ sample/src/main/java/com/github/devnied/emvnfccard/activity/testActivity.java
@@ -3,16 +3,66 @@ package com.github.devnied.emvnfccard.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.github.devnied.emvnfccard.R;
+import com.github.devnied.emvnfccard.adapter.MenuDrawerAdapter;
+import com.github.devnied.emvnfccard.fragment.AboutFragment;
+import com.github.devnied.emvnfccard.fragment.BillingFragment;
+import com.github.devnied.emvnfccard.fragment.CartFragment;
+import com.github.devnied.emvnfccard.fragment.CreateInventoryFragment;
+import com.github.devnied.emvnfccard.fragment.FundraiserFragment;
+import com.github.devnied.emvnfccard.fragment.IRefreshable;
+import com.github.devnied.emvnfccard.fragment.LogOutFragment;
+import com.github.devnied.emvnfccard.fragment.SimplePayFragment;
+import com.github.devnied.emvnfccard.fragment.ViewPagerFragment;
+import com.github.devnied.emvnfccard.utils.ConstantUtils;
 import com.github.devnied.emvnfccard.utils.CroutonUtils;
+
+import java.lang.ref.WeakReference;
+
+import de.keyboardsurfer.android.widget.crouton.Crouton;
 
 /**
  * Created by Sindri on 30/04/15.
  */
-public class testActivity extends FragmentActivity {
+public class testActivity extends FragmentActivity implements AdapterView.OnItemClickListener {
+    /**
+     * last selected Menu
+     */
+    private int mLastSelectedMenu = -1;
+    /**
+     * Reference for refreshable content
+     */
+    private WeakReference<IRefreshable> mRefreshableContent;
+    /**
+     * Action bar drawer toggle
+     */
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+
+    /**
+     * Drawer layout
+     */
+    private DrawerLayout mDrawerLayout;
+
+    /**
+     * Menu adapter
+     */
+    private MenuDrawerAdapter mMenuAdapter;
+
+
+    /**
+     * ListView drawer
+     */
+    private ListView mDrawerListView;
+    /* END DRAWER */
 
     String extra;
     @Override
@@ -30,10 +80,108 @@ public class testActivity extends FragmentActivity {
 
             }
         }, 1000);
+
+        /* Sidebar menu */
+        // get ListView defined in activity_main.xml
+        mDrawerListView = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mMenuAdapter = new MenuDrawerAdapter(this);
+        mDrawerListView.setAdapter(mMenuAdapter);
+        mDrawerListView.setOnItemClickListener(this);
+
+        // 2. App Icon
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // 2.1 create ActionBarDrawerToggle
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(/* */
+                this, /* host Activity */
+                mDrawerLayout, /* DrawerLayout object */
+                R.drawable.ic_drawer, /* nav drawer icon to replace 'Up' caret */
+                R.string.navigation_menu_open, /* "open drawer" description */
+                R.string.navigation_menu_close /* "close drawer" description */
+        );
+
+        // 2.2 Set actionBarDrawerToggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(true);
+        getActionBar().setDisplayUseLogoEnabled(false);
+        getActionBar().setDisplayShowCustomEnabled(true);
+    }
+
+    @Override
+    public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
+        if (mLastSelectedMenu != position) {
+            Fragment fragment = null;
+            switch (position) {
+                case ConstantUtils.CARDS_DETAILS:
+                    fragment = new ViewPagerFragment();
+                    refreshContent();
+                    break;
+                case ConstantUtils.SIMPLEPAY:
+                    fragment = new SimplePayFragment();
+                    break;
+                case ConstantUtils.CART:
+                    fragment = new CartFragment();
+                    break;
+                case ConstantUtils.INVERTORY:
+                    fragment = new CreateInventoryFragment();
+                    break;
+                case ConstantUtils.FUNDRAISER:
+                    fragment = new FundraiserFragment();
+                    break;
+                case ConstantUtils.ABOUT:
+                    fragment = new AboutFragment();
+                    break;
+                case ConstantUtils.LOGOUT:
+                    fragment = new LogOutFragment();
+                    break;
+                default:
+                    break;
+            }
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+            }
+            mLastSelectedMenu = position;
+        }
+        mDrawerLayout.closeDrawer(mDrawerListView);
+    }
+
+    private void refreshContent() {
+        if (mRefreshableContent != null && mRefreshableContent.get() != null) {
+            mRefreshableContent.get().update();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            Crouton.cancelAllCroutons();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (mLastSelectedMenu == ConstantUtils.ABOUT) {
+            Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.content_frame);
+            if (fragment != null) {
+                BillingFragment billing = (BillingFragment) fragment.getChildFragmentManager().findFragmentById(R.id.about_inapp_content);
+                if (billing != null) {
+                    billing.onActivityResult(requestCode, resultCode, data);
+                }
+            }
+        }
     }
 
     public void doagain(View view) {
         Intent intent = new Intent(this, SimplePayActivity.class);
         startActivity(intent);
     }
+
+
 }
